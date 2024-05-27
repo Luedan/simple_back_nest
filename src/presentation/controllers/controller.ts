36ff4542c69ae5ad55355bcb2a08fa63bd18f7ba
@Atencia <atencia@wowdesarrollos.com>
2024-05-27 15:55:49 -0500

    cambiando controller

---
+++ src/presentation/controllers/controller.ts
@@ -0,0 +1,16 @@
+import { DefaultController } from './default/default.controller';
+import { HealthController } from './health/health.controller';
+import { EmailParameterController } from './parameters/email/emailParameter.controller';
+import { S3ParameterController } from './parameters/s3/s3Parameter.controller';
+import { TodoController } from './todo/todo.controller';
+
+/**
+ * Array of controllers.
+ */
+export const CONTROLLERS = [
+  DefaultController,
+  HealthController,
+  TodoController,
+  S3ParameterController,
+  EmailParameterController,
+];
--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -5,20 +5,10 @@ import { ApplicationModule } from '@app/application/application.module';
 /**
  * Controllers
  */
-import { TodoController } from './controllers/todo/todo.controller';
-import { HealthController } from './controllers/health/health.controller';
-import { DefaultController } from './controllers/default/default.controller';
-import { S3ParameterController } from './controllers/parameters/s3/s3Parameter.controller';
-import { EmailParameterController } from './controllers/parameters/email/emailParameter.controller';
+import { CONTROLLERS } from './controllers/controller';
 
 @Module({
   imports: [ApplicationModule, TerminusModule],
-  controllers: [
-    DefaultController,
-    HealthController,
-    TodoController,
-    S3ParameterController,
-    EmailParameterController,
-  ],
+  controllers: [...CONTROLLERS],
 })
 export class PresentationModule {}
